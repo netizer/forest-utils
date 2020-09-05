006b334b833005b6c41f32df30663386153d7438
Krzysiek Herod <krzysiek.herod@gmail.com>
2020-09-05 16:51:59 +0200

    Check forest and groundcover stats

--- lib/stats.sh
+++ lib/stats.sh
@@ -1,12 +1,10 @@
-echo "Ruby code size"
-echo "* forest-rb"
-cd ../forest-rb
-find . | grep .rb | xargs cat | wc -l
+echo "Code sizes"
+cd ../
+echo "* Ruby"
+find . | grep "\.rb" | xargs cat | wc -l
 
-echo "* lamb-rb"
-cd ../lamb-rb
-find . | grep .rb | xargs cat | wc -l
+echo "* Forest"
+find . | grep "\.forest" | xargs cat | wc -l
 
-echo "* groundcover-rb"
-cd ../groundcover-rb
-find . | grep .rb | xargs cat | wc -l
+echo "* Groundcover"
+find . | grep "\.gc" | xargs cat | wc -l
